752f0b9f46dbbd10478d88317f08504aa7687ec4
Himanshu Kumar <himanshuecekumar@gmail.com>
2026-06-26 23:32:31 +0530

    login and signup working

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDb = async () => {
-    
-        console.log(process.env.MONGODB_URL);
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("DB connected")
-   
-}
-export default connectDb
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB Connected");
+  } catch (error) {
+    console.log("DB connection failed", error);
+  }
+};
+
+export default connectDb;
